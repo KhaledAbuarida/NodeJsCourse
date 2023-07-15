c57b5a769823535c9f80f898dad0a2cd59f8f8a8
KhaledAbuarida <khaledabuarida0@gmail.com>
2023-07-15 15:43:51 +0300

    enhancing the style of the shop list using Bstrap

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const errorController = require('./controllers/error');
 const app = express()
 app.use(bodyParser.urlencoded({extended: false}))
 
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
